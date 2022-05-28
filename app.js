f5b28ad4d6cb44d405825f3ce6de86301a4ecd11
the69nemo <thestigmata@yandex.ru>
2022-05-28 19:05:37 +0300

    done

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ const handleError = require('./middlewares/handleError');
 
 require('dotenv').config();
 
-const { PORT = 3000, DB_BASE } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(DB_BASE, () => {
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', () => {
   // eslint-disable-next-line no-console
-  console.log('**********Подключено к Базе**********');
+  console.log(`*****Подключено к порту: ${PORT}**********`);
 });
 
 app.use(requestLogger);
